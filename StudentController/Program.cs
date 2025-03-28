072341a291cfadf3901292cd05a53f3d69152bc6
NguyenHaii <167160632+NguyenHaii@users.noreply.github.com>
2025-03-28 10:38:56 +0700

    Add files via upload

--- StudentController/Program.cs
+++ StudentController/Program.cs
@@ -1,49 +1,113 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Globalization;
 
-namespace StudentController
+interface IRepository<T>
 {
-    class Program
+    void Add(T item);
+    List<T> GetAll();
+    void Update(int id, T item);
+    void Delete(int id);
+}
+
+class Repository<T> : IRepository<T> where T : IEntity
+{
+    private readonly List<T> _items = new List<T>();
+
+    public void Add(T item)
     {
-        static void Main()
+        _items.Add(item);
+    }
+
+    public List<T> GetAll()
+    {
+        return _items;
+    }
+
+    public void Update(int id, T item)
+    {
+        var existingItem = _items.FirstOrDefault(x => x.Id == id);
+        if (existingItem != null)
         {
-            Console.OutputEncoding = Encoding.UTF8;
-            StudentController controller = new StudentController();
+            _items[_items.IndexOf(existingItem)] = item;
+        }
+    }
 
-            while (true)
+    public void Delete(int id)
+    {
+        var item = _items.FirstOrDefault(x => x.Id == id);
+        if (item != null)
+        {
+            _items.Remove(item);
+        }
+    }
+}
+
+interface IEntity
+{
+    int Id { get; set; }
+}
+
+class Student : IEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IRepository<Student> studentRepo = new Repository<Student>();
+        while (true)
+        {
+            Console.WriteLine("\nMenu:");
+            Console.WriteLine("1. Add Student");
+            Console.WriteLine("2. List Students");
+            Console.WriteLine("3. Update Student");
+            Console.WriteLine("4. Delete Student");
+            Console.WriteLine("5. Exit");
+            Console.Write("Choose an option: ");
+
+            string choice = Console.ReadLine();
+            switch (choice)
             {
-                Console.WriteLine("\n===== MENU =====");
-                Console.WriteLine("1. Nhập thông tin sinh viên");
-                Console.WriteLine("2. Hiển thị danh sách sinh viên");
-                Console.WriteLine("3. Tìm kiếm sinh viên theo môn học");
-                Console.WriteLine("4. Tìm kiếm sinh viên theo giới tính");
-                Console.WriteLine("5. Thoát");
-                Console.Write("Chọn chức năng: ");
-
-                string choice = Console.ReadLine();
-                switch (choice)
-                {
-                    case "1":
-                        controller.AddStudent();
-                        break;
-                    case "2":
-                        controller.DisplayStudents();
-                        break;
-                    case "3":
-                        controller.SearchBySubject();
-                        break;
-                    case "4":
-                        controller.SearchByGender();
-                        break;
-                    case "5":
-                        return;
-                    default:
-                        Console.WriteLine("Lựa chọn không hợp lệ, vui lòng nhập lại!");
-                        break;
-                }
+                case "1":
+                    Console.Write("Enter ID: ");
+                    int id = int.Parse(Console.ReadLine());
+                    Console.Write("Enter Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Enter Age: ");
+                    int age = int.Parse(Console.ReadLine());
+                    studentRepo.Add(new Student { Id = id, Name = name, Age = age });
+                    break;
+                case "2":
+                    var students = studentRepo.GetAll();
+                    foreach (var student in students)
+                    {
+                        Console.WriteLine($"ID: {student.Id}, Name: {student.Name}, Age: {student.Age}");
+                    }
+                    break;
+                case "3":
+                    Console.Write("Enter ID to update: ");
+                    int updateId = int.Parse(Console.ReadLine());
+                    Console.Write("Enter New Name: ");
+                    string newName = Console.ReadLine();
+                    Console.Write("Enter New Age: ");
+                    int newAge = int.Parse(Console.ReadLine());
+                    studentRepo.Update(updateId, new Student { Id = updateId, Name = newName, Age = newAge });
+                    break;
+                case "4":
+                    Console.Write("Enter ID to delete: ");
+                    int deleteId = int.Parse(Console.ReadLine());
+                    studentRepo.Delete(deleteId);
+                    break;
+                case "5":
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Try again.");
+                    break;
             }
         }
     }
